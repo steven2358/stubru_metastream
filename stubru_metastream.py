56d81d13b1bcaff8fa85cc3541aa6f1c7a908354
Steven <steven2358@gmail.com>
2013-06-26 12:01:16 +0200

    empty tags for no-music broadcast
    added empty tags for no-music broadcast and reduced the sleep time

--- stubru_metastream.py
+++ stubru_metastream.py
@@ -12,6 +12,8 @@ while 1:
 		print "TITLE=" + data["noa"]["songs"][0]["title"] + "\nARTIST=" + data["noa"]["songs"][0]["artist"] + "\n."
 		sys.stdout.flush()
 	else:
+		# broadcast is not music
+		print "TITLE=\nARTIST=\n."
 		sys.stdout.flush()
 
-	time.sleep(10)
+	time.sleep(5)
