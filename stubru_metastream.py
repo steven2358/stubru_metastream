f40c6aa81e5daaf3f3d0c6f952b9056dd86e3ec5
Steven <steven2358@gmail.com>
2014-09-09 13:35:20 +0200

    update for new noa_url

--- stubru_metastream.py
+++ stubru_metastream.py
@@ -3,17 +3,32 @@ import urllib2
 import time
 import sys
 
-noa_url = 'http://services.vrt.be/epg/playlists/now-on-air?channel_code=41&accept=application%2Fvnd.epg.vrt.be.onair_1.1%2Bjson'
+noa_url = 'http://services.vrt.be/playlist/onair?channel_code=41&accept=application%2Fvnd.playlist.vrt.be.noa_1.0%2Bjson'
 
 while 1:
-	data = json.load(urllib2.urlopen(noa_url))
+  data = json.load(urllib2.urlopen(noa_url))
 	
-	if data["noa"]["songs"]:
-		print "TITLE=" + data["noa"]["songs"][0]["title"] + "\nARTIST=" + data["noa"]["songs"][0]["artist"] + "\n."
-		sys.stdout.flush()
-	else:
-		# broadcast is not music
-		print "TITLE=\nARTIST=\n."
-		sys.stdout.flush()
+  onairs = data["onairs"];
+  
+  success = 0
+  if onairs:
+    for onair in onairs:
+      if onair["type"] == "SONG" and onair["onairType"] == "NOW":
+        for prop in onair["properties"]:
+          if prop["key"] == "ARTISTNAME":
+            artist = prop["value"]
+          elif prop["key"] == "TITLE":
+            title = prop["value"]
+        
+        if artist and title:
+          success = 1
 
-	time.sleep(10)
+  if success:
+    print "TITLE=" + title + "\nARTIST=" + artist + "\n."
+    sys.stdout.flush()
+  else:
+    # broadcast is not music
+    print "TITLE=none\nARTIST=\n."
+    sys.stdout.flush()
+
+  time.sleep(10)
